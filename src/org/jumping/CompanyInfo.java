b3e0591ca9a7cd94e33474042ad568c021972fd1
boopathypradeep <boopathypradeepr@gmail.com>
2022-04-28 19:02:46 +0530

    jumping statements

---
+++ src/org/jumping/CompanyInfo.java
@@ -0,0 +1,17 @@
+package org.jumping;
+
+public class CompanyInfo {
+	public CompanyInfo(String name) {
+		System.out.println("Company name is:"+name);
+	}
+	public CompanyInfo(int id) {
+		System.out.println("Company name is:"+id);
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
---
+++ src/org/jumping/Enjoy.java
@@ -0,0 +1,13 @@
+package org.jumping;
+
+public class Enjoy extends CompanyInfo {
+
+public Enjoy(String name) {
+	super(name);// TODO Auto-generated constructor stub
+}
+public Enjoy(int id) {
+	// TODO Auto-generated constructor stub
+}
+	
+	
+}
