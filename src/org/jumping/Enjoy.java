fd70f5eef873b274e2f6b95dc7ea63c7fad3badc
boopathypradeep <boopathypradeepr@gmail.com>
2022-04-29 15:50:24 +0530

    updated jumping statements

--- src/org/jumping/Enjoy.java
+++ src/org/jumping/Enjoy.java
@@ -3,11 +3,16 @@ package org.jumping;
 public class Enjoy extends CompanyInfo {
 
 public Enjoy(String name) {
-	super(name);// TODO Auto-generated constructor stub
+	super(name);
 }
-public Enjoy(int id) {
-	// TODO Auto-generated constructor stub
-}
-	
+
+	private void hello() {
+		// TODO Auto-generated method stub
+
+	}
+	private void hai() {
+		// TODO Auto-generated method stub
+
+	}
 	
 }
